79a4d23f269df557b69a21aaeddbf419f01c6c0a
Sergey Mezentsev <thebits@yandex-team.ru>
2012-10-06 18:03:04 +0400

    Функция обходит дерево начиная с body

--- addon/data/inject.js
+++ addon/data/inject.js
@@ -1,1 +1,26 @@
 // Файл работает в контексте веб-страницы
+
+// функция проходит все дерево
+var result = [];
+var root = document.body;
+
+var node = root.childNodes[0];
+while(node != null) {
+    if (node.nodeType == Node.TEXT_NODE) {
+        result.push(node.textContent);
+    }
+
+    if(node.hasChildNodes()) {
+        node = node.firstChild;
+    } else {
+        while(node.nextSibling == null && node != root) {
+            node = node.parentNode;
+        }
+        node = node.nextSibling;
+    }
+}
+
+result.forEach(function(r) {
+    console.log("!!!");
+    console.log(r);
+});
