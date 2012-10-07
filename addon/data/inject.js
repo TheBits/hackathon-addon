206eab5f7dd231c2d468f90986b49216497f0179
Sergey Mezentsev <thebits@yandex-team.ru>
2012-10-07 13:00:49 +0400

    Рабочая версия

--- addon/data/inject.js
+++ addon/data/inject.js
@@ -1,18 +1,24 @@
 // Файл работает в контексте веб-страницы
 
 self.port.on('result', function(message) {
-    console.log('message', message);
-    var messages = JSON.parse(message);
+    // console.log('message', message);
+    // console.log('message', JSON.parse(message)[1][0]['start']);
+    var messages;
+    try {
+        messages = JSON.parse(message);
+    } catch(e) {
+        // console.log('Error: '+e);
+        // console.log('message', message);
+        return;
+    }
 
     var index = 0;
+    var css =[];
     var root = document.body;
     var node = root.childNodes[0];
+
     while(node != null) {
-        if (node.nodeType == Node.TEXT_NODE) {
-            var data = message[index];
-            index += 1;
-            // node.textContent;
-        }
+        var prev = node.nodeType == Node.TEXT_NODE? node: null;
 
         if (node.hasChildNodes()  && node.tagName !== 'SCRIPT') {
             node = node.firstChild;
@@ -22,7 +28,39 @@ self.port.on('result', function(message) {
             }
             node = node.nextSibling;
         }
+
+        if (prev !== null) {
+            var data = messages[index];
+            index += 1;
+
+            if (data === void 0) {
+                continue;
+            };
+
+            var content = prev.textContent;
+
+            for (var i = data.length - 1; i >= 0; i--) {
+                item = data[i];
+                console.log('item', item);
+                console.log('item', item['image']);
+                content = [
+                    content.substr(0, item['start']),
+                    '<span class="yana-highlight">',
+                    content.substr(item['start'], item['length']),
+                    '<span><img style="float:left" src="'+item['data']['image']+'">'+item['data']['text']+'</span>',
+                    '</span>',
+                    content.substr(item['length']+item['start'], content.length)
+                ].join(' ');
+            };
+
+            // Заменить тег
+            var el = document.createElement('span');
+            el.innerHTML = content;
+            el.className = "yana-expand";
+            prev.parentNode.replaceChild(el, prev);
+        }
     }
+    console.log('result merged');
 });
 
 // Получает текстовые данные из DOM
@@ -31,6 +69,7 @@ var root = document.body;
 var node = root.childNodes[0];
 while(node != null) {
     if (node.nodeType == Node.TEXT_NODE) {
+        // console.log(node.textContent)
         result.push(node.textContent);
     }
 
--- addon/lib/main.js
+++ addon/lib/main.js
@@ -8,16 +8,39 @@ pageMod.PageMod({
   include: '*',
   contentScriptWhen: 'end',
   contentScriptFile: data.url('inject.js'),
+  contentStyle: [
+    // "span.yana-expand { padding: 0; margin 0}",
+    ".yana-highlight { text-decoration: underline }",
+    ".yana-highlight span {\
+      background-color: #FFFF73;\
+      border-radius: 5px;\
+      display: none;\
+      opacity: 0.9;\
+      padding: 10px;\
+      z-index: 100;\
+      box-shadow: 0 40px 60px -30px #FFFFFF inset, 0 0 20px rgba(0, 0, 0, 0.2);\
+    }",
+    ".yana-highlight:hover span { display: block }"
+  ],
   onAttach: function(worker) {
     worker.port.on('content', function(content) {
       // Получить контент страницы
       // console.log(JSON.stringify(content));
+      console.log('send list: ' + content.length)
       Request({
         url: prefs.service_url,
         content: JSON.stringify(content),
         contentType: "application/json",
         onComplete: function (response) {
           worker.port.emit('result', response.text);
+            try {
+                messages = JSON.parse(response.text);
+            } catch(e) {
+                console.log('Error: '+e);
+                console.log('message', response.text);
+                console.log('message', content);
+                return;
+            }
         }
       }).post();
     });
